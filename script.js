7680e595c364516f4646ea1f17091118853f13bc
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2026-01-09 17:20:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let result = "";
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    result += "FizzBuzz";
+  } else if (i % 3 === 0) {
+    result += "Fizz";
+  } else if (i % 5 === 0) {
+    result += "Buzz";
+  } else {
+    result += i;
+  }
+
+  if (i !== 100) {
+    result += "\n";
+  }
+}
+
+alert(result);
